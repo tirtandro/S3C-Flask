b313746441c6c7930ce9d1de824cb63d68971ea6
tirtandro <tirtandro@gmail.com>
2026-03-29 09:48:36 +0700

    debug bad gateway

--- app.py
+++ app.py
@@ -813,6 +813,6 @@ with app.app_context():
     migrate_db()
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8080))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=5000)
+
 
